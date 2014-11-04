e90f3ce90876122419982cd0ed34295bdcd9da7b
FikretPythonLesson <FikretPythonLesson@users.noreply.github.com>
2014-11-04 14:12:39 +0200

    Create multiply2.py
    Multiply function, values are obtained from user

---
+++ multiply2.py
@@ -0,0 +1,13 @@
+def multi(a, b):     
+    answer = a * b   
+    return answer    
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter second number: "))
+           
+q1 = multi(num1, num2)   
+print(q1)            
+
+
+
+
