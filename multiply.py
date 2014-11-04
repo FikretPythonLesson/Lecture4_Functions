236c74b956b4fa219006b57b5d50fd6f4a9268d5
FikretPythonLesson <FikretPythonLesson@users.noreply.github.com>
2014-11-04 14:12:02 +0200

    Create multiply.py
    Function practice with multiply

---
+++ multiply.py
@@ -0,0 +1,9 @@
+def multi(a, b):     # a and b are spaces that must be filled by two numbers
+    answer = a * b   # Times a and b and putting it into variable answer
+    return answer    # Returning variable answer to caller
+
+q1 = multi(5, 100)   # Create variable q1 and put function multi with 5 replacing a 100
+print(q1)            # and 100 replacing b into q1. Finally print q1
+
+
+
