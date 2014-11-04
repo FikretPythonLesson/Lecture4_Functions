48394d2d73b4b5e1add1cf2f9c82dc3edc8f48ee
FikretPythonLesson <FikretPythonLesson@users.noreply.github.com>
2014-11-04 14:09:53 +0200

    Create LetterA_F.py
    Printing letter A and F with turtle.

---
+++ LetterA_F.py
@@ -0,0 +1,12 @@
+import turtle
+import letters
+
+letters.drawLetter('A', 100, 50, 0)
+
+turtle.penup()
+turtle.forward(100)
+turtle.pendown()
+
+letters.drawLetter('F', 100, 50, 0)
+
+turtle.done()
